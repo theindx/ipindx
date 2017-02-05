cce48f5fec2b38b6f6c3b7b981bea15e37b33301
Zack Piper <zack@apertron.net>
2017-02-05 10:17:17 +0000

    Starting anew.

--- scripts/ips.sh
+++
@@ -1,8 +0,0 @@
-tmp=$(mktemp -d)
-wget $1 -O $tmp/log.txt
-cat $tmp/log.txt | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | uniq >> $tmp/ips.txt
-while read ip; do
-    con="$(grep $ip $tmp/log.txt)"
-    IP=$ip CONTENTS="$con" erb ips.erb >> ips.cson
-done <$tmp/ips.txt
-
